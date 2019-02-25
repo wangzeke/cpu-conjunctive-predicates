b6be16ac3053c262fa5001bddc3bc9d4c7811a66
wangzeke <wangzeke@zju.edu.cn>
2019-02-25 15:10:47 +0100

    stpr: support exec_model = 1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+complex_predicates: includes the hebe implementation.
 
+ByteSlice-master: Clone from https://github.com/fzqneo/ByteSlice.
+
+tpch_table: Use MonetDB to generate the denormalized table.
+
+backup: something maybe useful.
--- benchmark/stpr.cpp
+++ benchmark/stpr.cpp
@@ -40,7 +40,7 @@
 #include "perf_counters.h"
 
 
-//#define INTEL_PCM_ENABLE
+#define INTEL_PCM_ENABLE
 
 
 	
@@ -88,8 +88,9 @@ void main(int argc, char **argv)
 {
 	uint64_t tuples           = 1024*1024*1024; //
 	uint32_t sel_table_size   = 16*1024;
-	uint32_t  inner_loop_size = argc > 1? atoi(argv[1]) : tuples;
-	printf("\ntuples = %d,  inner_loop_size = %d\n", tuples, inner_loop_size); 
+	uint32_t  exec_model      = argc > 1? atoi(argv[1]) : 0;
+	uint32_t  inner_loop_size = argc > 2? atoi(argv[2]) : tuples;
+	printf("tuples = %d,  inner_loop_size = %d\n", tuples, inner_loop_size); 
 
 	//initlize random number seed...
 	int seed       = rand();
@@ -105,12 +106,12 @@ void main(int argc, char **argv)
     //uint64_t T1_len_aligned = ( ((tuples + 63)>>6)<<6 ); //upper boundary to 64-byte alignment.
 	
     ///////////////////////allocate space for input data where we perform stpr////////////////////	
-    original     = (ByteUnit *) malloc_memory(tuples*sizeof(AvxUnit), false);//use 4K page.	
+    original     = (ByteUnit *) malloc_memory(tuples*sizeof(ByteUnit), false);//use 4K page.	
     if (original == NULL)     {
           printf ("input original_malloc fails\n");
           return;
     }
-    for(i=0; i < tuples*32; i+=8){
+    for(i=0; i < tuples; i+=8){
       original[i] =  1;
     }
    
@@ -128,13 +129,13 @@ void main(int argc, char **argv)
 	
 
 	double f_result[200];
-	float selectivity_tmp = 0.1;
+	float selectivity_tmp = 0.04;
   
   
-	//for (int jj = 0; jj <= 100; jj++)
+	for (int jj = 0; jj < 25; jj++)
 	{   
 		selectivity      = selectivity_tmp;
-		selectivity_tmp  = selectivity_tmp + 0.01;
+		selectivity_tmp  = selectivity_tmp + 0.04;
 		uint32_t literal =  (uint32_t)( selectivity*(float)(1<<8) ); //do greater than literal...
 		
 	#ifdef INTEL_PCM_ENABLE		
@@ -149,37 +150,79 @@ void main(int argc, char **argv)
 	
 	   	//log the start time for stpr.
 	   	uint64_t t1 = thread_time(); //
-		
-		//main body of stpr benchmark...with 256-bit AVX2...		  
-	   	for (uint64_t i = 0; i < tuples/32; i++)
-		{   
-			if (selectivity_array[i&(sel_table_size-1)] < literal) 
-			{
-				//AvxUnit m_equal      = avx_ones();
-				//AvxUnit mask_greater = avx_zero();
-				//AvxUnit m_data_in    = avx_load( (ByteUnit *)&original[(i*32)&(inner_loop_size-1)] ); //
-	   			//mask_greater         = avx_or(mask_greater, avx_and(m_equal, avx_cmpgt<ByteUnit>(m_data_in, m_constant)));
-				//m_sum                = avx_or(m_sum, mask_greater); 
-				AvxUnit m_data_in    = avx_load( (ByteUnit *)&original[(i*32)&(inner_loop_size-1)] ); //
-				m_sum                = avx_or( m_sum, avx_cmpgt<ByteUnit>(m_data_in, m_constant) ); 
-				sel_counter++;
+		if (exec_model == 0)
+		{
+	   		for (uint64_t i = 0; i < tuples/32; i++)
+			{   
+				if (selectivity_array[i&(sel_table_size-1)] < literal) 
+				{
+					//AvxUnit m_equal      = avx_ones();
+					//AvxUnit mask_greater = avx_zero();
+					//AvxUnit m_data_in    = avx_load( (ByteUnit *)&original[(i*32)&(inner_loop_size-1)] ); //
+	   				//mask_greater         = avx_or(mask_greater, avx_and(m_equal, avx_cmpgt<ByteUnit>(m_data_in, m_constant)));
+					//m_sum                = avx_or(m_sum, mask_greater); 
+					AvxUnit m_data_in    = avx_load( (ByteUnit *)&original[(i*32)&(inner_loop_size-1)] ); //
+					m_sum                = avx_or( m_sum, avx_cmpgt<ByteUnit>(m_data_in, m_constant) ); 
+					sel_counter++;
+				}
 			}
 		}
+		else if (exec_model == 1)
+		{
+	   		AvxUnit  m_sum_0      = avx_zero(); uint64_t sel_counter_0 = 0;
+	   		AvxUnit  m_sum_1      = avx_zero(); uint64_t sel_counter_1 = 0;
+	   		AvxUnit  m_sum_2      = avx_zero(); uint64_t sel_counter_2 = 0;
+	   		AvxUnit  m_sum_3      = avx_zero(); uint64_t sel_counter_3 = 0;
+	   		for (uint64_t i = 0; i < tuples/32; i += 4)
+			{   
+				if (selectivity_array[(i+0)&(sel_table_size-1)] < literal) 
+				{
+					AvxUnit m_data_in    = avx_load( (ByteUnit *)&original[((i+0)*32)&(inner_loop_size-1)] ); //
+					m_sum_0              = avx_or( m_sum_0, avx_cmpgt<ByteUnit>(m_data_in, m_constant) ); 
+					sel_counter_0++;
+				}
+				if (selectivity_array[(i+1)&(sel_table_size-1)] < literal) 
+				{
+					AvxUnit m_data_in    = avx_load( (ByteUnit *)&original[((i+1)*32)&(inner_loop_size-1)] ); //
+					m_sum_1              = avx_or( m_sum_1, avx_cmpgt<ByteUnit>(m_data_in, m_constant) ); 
+					sel_counter_1++;
+				}
+				if (selectivity_array[(i+2)&(sel_table_size-1)] < literal) 
+				{
+					AvxUnit m_data_in    = avx_load( (ByteUnit *)&original[((i+2)*32)&(inner_loop_size-1)] ); //
+					m_sum_2              = avx_or( m_sum_2, avx_cmpgt<ByteUnit>(m_data_in, m_constant) ); 
+					sel_counter_2++;
+				}
+				if (selectivity_array[(i+0)&(sel_table_size-1)] < literal) 
+				{
+					AvxUnit m_data_in    = avx_load( (ByteUnit *)&original[((i+3)*32)&(inner_loop_size-1)] ); //
+					m_sum_3              = avx_or( m_sum_3, avx_cmpgt<ByteUnit>(m_data_in, m_constant) ); 
+					sel_counter_3++;
+				}								
+			}
+
+			m_sum       = avx_or( avx_or(m_sum_0, m_sum_1), avx_or(m_sum_2, m_sum_3) );
+			sel_counter = sel_counter_0 + sel_counter_1 + sel_counter_2 + sel_counter_3;
+		}
+		else 
+		{
+			printf("the current execution model: %d is not supported. \n", exec_model);
+			return;
+		}
+
 	    uint32_t u_sum = _mm256_movemask_epi8(m_sum);  
 	
 	   	//log the end time for stpr.
 	    t1 = thread_time() - t1;
 	
 	#ifdef INTEL_PCM_ENABLE			
-	        PCM_stop();
-	        printf("=====print the profiling result==========tuples = %d, real selectivity = %f\n", tuples, (float)sel_counter/(float)tuples);
-	        PCM_printResults();		
-			PCM_cleanup();
-			//printf("real selectivity = %f\n", (float)counter/(float)T1_len);
+		PCM_stop();
+		printf("=====print the profiling result==========tuples = %d, real selectivity = %f\n", tuples, (float)sel_counter/(float)(tuples/32) );
+		PCM_printResults();		
+		PCM_cleanup();
 	#endif
 		//f_result[jj] = (double)t1/(double)(tuples/32);
-	     //printf("time required for each code is %f, for each executed code: %f\n\n", (double)t1/(double)tuples, (double)t1/((double)counter) );
-		 printf(" (%d) time required for each code is %f\n", u_sum, (double)t1/(double)(tuples/32) );
+		printf("(%d) time required for each code is %f ns/code \n", u_sum, (double)t1/(double)tuples );
 	}
 
 
