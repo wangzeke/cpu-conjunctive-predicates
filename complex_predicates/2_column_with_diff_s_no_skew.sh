1318185ef32cfb53ccb49e617f30df2251d92b8e
wangzeke <wangzeke@zju.edu.cn>
2019-10-01 11:07:19 +0200

    add the script for the uniform dataset

--- complex_predicates/2_column_with_diff_s_no_skew.sh
+++ complex_predicates/2_column_with_diff_s_no_skew.sh
@@ -6,47 +6,50 @@ for i in 1 #5 5 7   1 4 8 16
 do
   echo ""  >>$1 
   echo "With threads:" $i >>$1
-	    for j in 396 924 1057 2244 4092 4500
-	    do
-	  echo "1_Literal of p2:" $j >>$1
-          ./2_column_compare_with_literal_test_skewed.x $i 0 17 17 0 1 1 0.5 0.5 $j >result.txt
+	    for j in 0.05 0.04 0.03 0.004 0.0003
+		do
+		  echo "1, selectivity of p2:" $j >>$1
+          	./2_column_compare_with_literal_test.x $i 1 17 17 0 1 1 0.5 $j >result.txt
           cat result.txt |grep "p_s_model"  >>$1
           cat result.txt |grep "codes_per_ns" >>$1
           cat result.txt |grep "real:" >>$1
           echo ""  >>$1	
           rm result.txt
 
-	  echo "2_Literal of p2:" $j >>$1
-          ./2_column_compare_with_literal_test_skewed.x $i 0 17 17 0 1 1 0.5 0.5 $j >result.txt
+		  echo "2, selectivity of p2:" $j >>$1
+          ./2_column_compare_with_literal_test.x $i 1 17 17 0 1 1 0.5 $j >result.txt
           cat result.txt |grep "p_s_model"  >>$1
           cat result.txt |grep "codes_per_ns" >>$1
           cat result.txt |grep "real:" >>$1
           echo ""  >>$1	
           rm result.txt
+
+		  echo "3, selectivity of p2:" $j >>$1
+          ./2_column_compare_with_literal_test.x $i 1 17 17 0 1 1 0.5 $j >result.txt
+          cat result.txt |grep "p_s_model"  >>$1
+          cat result.txt |grep "codes_per_ns" >>$1
+          cat result.txt |grep "real:" >>$1
+          echo ""  >>$1	
+          rm result.txt
+
+		  echo "4, selectivity of p2:" $j >>$1
+          ./2_column_compare_with_literal_test.x $i 1 17 17 0 1 1 0.5 $j >result.txt
+          cat result.txt |grep "p_s_model"  >>$1
+          cat result.txt |grep "codes_per_ns" >>$1
+          cat result.txt |grep "real:" >>$1
+          echo ""  >>$1	
+          rm result.txt
+
+		  echo "5, selectivity of p2:" $j >>$1
+          ./2_column_compare_with_literal_test.x $i 1 17 17 0 1 1 0.5 $j >result.txt
+          cat result.txt |grep "p_s_model"  >>$1
+          cat result.txt |grep "codes_per_ns" >>$1
+          cat result.txt |grep "real:" >>$1
+          echo ""  >>$1	
+          rm result.txt
+
+
+		done  
 		
-	  echo "3_Literal of p2:" $j >>$1
-          ./2_column_compare_with_literal_test_skewed.x $i 0 17 17 0 1 1 0.5 0.5 $j >result.txt
-          cat result.txt |grep "p_s_model"  >>$1
-          cat result.txt |grep "codes_per_ns" >>$1
-          cat result.txt |grep "real:" >>$1
-          echo ""  >>$1	
-          rm result.txt
-		
-	  echo "4_Literal of p2:" $j >>$1
-          ./2_column_compare_with_literal_test_skewed.x $i 0 17 17 0 1 1 0.5 0.5 $j >result.txt
-          cat result.txt |grep "p_s_model"  >>$1
-          cat result.txt |grep "codes_per_ns" >>$1
-          cat result.txt |grep "real:" >>$1
-          echo ""  >>$1	
-          rm result.txt
-		
-	  echo "5_Literal of p2:" $j >>$1
-          ./2_column_compare_with_literal_test_skewed.x $i 0 17 17 0 1 1 0.5 0.5 $j >result.txt
-          cat result.txt |grep "p_s_model"  >>$1
-          cat result.txt |grep "codes_per_ns" >>$1
-          cat result.txt |grep "real:" >>$1
-          echo ""  >>$1	
-          rm result.txt
-	   done  
-								 
+		 
      done	   
